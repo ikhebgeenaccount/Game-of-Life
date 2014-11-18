5d7477b797a23ebf003f905e229864ff3a2a5f00
ikhebgeenaccount <lars.abbo@gmail.com>
2014-11-18 17:08:40 +0100

    Added source for check if image is clicked.

--- Game of Life/src/com/ihga/main/Main.java
+++ Game of Life/src/com/ihga/main/Main.java
@@ -1,5 +1,7 @@
 package com.ihga.main;
 
 public class Main {
+	//Detect if image is clicked:
+	//	http://stackoverflow.com/questions/7454569/how-to-detect-a-click-event-in-an-image-class-in-java
 
 }
