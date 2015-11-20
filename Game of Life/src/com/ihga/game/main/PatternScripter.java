a714ed2d518af8d81094cc173f9c9aeb4118a5b2
ikhebgeenaccount <lars.abbo@gmail.com>
2015-11-20 11:54:36 +0100

    Added PatternScripter

---
+++ Game of Life/src/com/ihga/game/main/PatternScripter.java
@@ -0,0 +1,37 @@
+package com.ihga.game.main;
+
+public class PatternScripter {
+	
+	private static String delimiter = ContentPanel.getDelimiter();
+	
+	public static void main(String[] args){
+		System.out.println(generateRandom(7500));
+	}
+	
+	public static String getStraightLine(int y){
+		String res = "";
+		for(int i = 0; i < ContentPanel.getFieldWidth(); i++){
+			res += i + "," + y + delimiter;
+		}
+		return res;
+	}
+	
+	public static String generateRandom(int number){
+		String res = "";
+		for(int i = 0; i < number; i++){
+			int x = Integer.MAX_VALUE, y = Integer.MAX_VALUE;
+			
+			while(x >= ContentPanel.getFieldWidth()){
+				x = (int)Math.round(Math.random() * ContentPanel.getFieldWidth());
+			}
+			
+			while(y >= ContentPanel.getFieldHeight()){
+				y = (int)Math.round(Math.random() * ContentPanel.getFieldHeight());
+			}			
+			
+			res += x + "," + y + delimiter;
+		}
+		return res;
+	}
+
+}
